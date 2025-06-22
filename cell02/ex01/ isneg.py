022dfa7fe5a0012275df6fd6f8a7a1aba0795871
angelinaamandaadu <angelinaamandaadu@gmail.com>
2025-06-22 07:35:32 +0000

    commit

---
+++ cell02/ex01/ isneg.py
@@ -0,0 +1,9 @@
+user_input = input ("Enter a number: ")
+number = float(user_input)
+
+if number < 0:
+    print("This number is negative.")
+elif number > 0:
+    print("This number is positive.") 
+else:
+    print("This number is both positive and negative.")
