a1d736ea3a4c4bc7643d57c14c5b38560b84468c
angelinaamandaadu <angelinaamandaadu@gmail.com>
2025-06-22 08:31:44 +0000

    commented cell03ex01

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,2 +1,8 @@
 #Multiplication table Exercise 01 cell 03
 number = int(input("Enter a number: "))
+print (number)
+i = 1
+while i <=12:
+    product = number * 1
+    print (f"{number} x {i} = {product}")
+    i += 1
