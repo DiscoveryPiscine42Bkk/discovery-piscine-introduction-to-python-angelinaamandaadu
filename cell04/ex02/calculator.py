2efa213a72ce0245d22e1853366f7eab0e9ca6da
angelinaamandaadu <angelinaamandaadu@gmail.com>
2025-06-23 09:41:06 +0000

    fix work

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-#Ask the user for two numbers
+
 first = float(input("Give me the first number: "))
 second = float(input("Give me the second number: "))
-#Thank the user
+
 print("Thank you!")
-#Perform and display the calculations
+
 print(f"{first} + {second} = {first + second}")
 print(f"{first} - {second} = {first - second}")
 print(f"{first} / {second} = {first / second}")
